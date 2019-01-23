9198f9e012bbd89771a8d23c6ef7c3414d3c5a64
sd.trent@yahoo.com <sd.trent@yahoo.com>
2019-01-23 15:45:10 -0700

    Initial Commit

---
+++ GitTut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTut
+//
+//  Created by Sam Trent on 1/23/19.
+//  Copyright © 2019 Sam Trent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
